db14c84e4a35ab4355b710f7078a9f8b6c70e069
nakjw0619 <helen.kim@hotmail.com>
2015-10-18 23:41:38 +0900

    Add basic database default config
    Showing format

---
+++ db_config.php
@@ -0,0 +1,14 @@
+<?php
+//db_config.php
+ 
+/*
+ * All database connection variables
+ */
+ 
+//define('DB_USER', "root"); // db user
+//define('DB_PASSWORD', ""); // db password (mention your db password here)
+//define('DB_DATABASE', "database"); // database name
+//define('DB_SERVER', "localhost"); // db server
+
+?>
+
